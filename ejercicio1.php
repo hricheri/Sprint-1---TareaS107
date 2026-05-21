29994b8201475c08c3befb2734f3bc00b28f44e1
hricheri <huilen.richeri@gmail.com>
2026-05-21 13:05:47 +0200

    Correcciones Ejercicio 1

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,10 +1,25 @@
 <?php
 
-try {
-    // ingreso de datos
-    $numero = readline("Introduce el número a dividir: ");
-    $divisor = readline("Introduce el divisor: ");
+// ingreso de datos
+$numero = readline("Introduce el número a dividir: ");
+$divisor = readline("Introduce el divisor: ");
 
+try {
+    $resultado = calculator($numero, $divisor);
+    echo "Resultado: $resultado\n";
+
+} catch (Exception $excepcionGeneral) {
+    echo "Error: " . $excepcionGeneral->getMessage() . "\n";
+}
+
+function calculator($numero, $divisor) {
+    checkInputs($numero, $divisor);
+
+    // operación
+    return $numero / $divisor;
+}
+
+function checkInputs($numero, $divisor) {
     // validacion de números
     if (!is_numeric($numero) || !is_numeric($divisor)) {
         throw new Exception("Los números no son válidos, intenta de nuevo");
@@ -14,14 +29,6 @@ try {
     if ($divisor == 0) {
         throw new Exception("No se puede dividir por cero");
     }
-
-    // operación
-    $resultado = $numero / $divisor;
-
-    echo "Resultado: $resultado\n";
-
-} catch (Exception $excepcionGeneral) {
-    echo "Error: " . $excepcionGeneral->getMessage() . "\n";
 }
 
 ?>
